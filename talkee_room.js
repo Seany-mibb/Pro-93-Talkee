9b4bd7c49f3a95dd6425ba01442a1a116faf7e56
Seany-mibb <86206802+Seany-mibb@users.noreply.github.com>
2022-04-05 20:10:17 -0700

    Add files via upload

--- talkee_room.js
+++ talkee_room.js
@@ -1,5 +1,17 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyAItiqLaEsKioh5i1S1ddLW0Awmx7PcDXg",
+      authDomain: "huzzah-7dcfa.firebaseapp.com",
+      databaseURL: "https://huzzah-7dcfa-default-rtdb.firebaseio.com",
+      projectId: "huzzah-7dcfa",
+      storageBucket: "huzzah-7dcfa.appspot.com",
+      messagingSenderId: "209037821998",
+      appId: "1:209037821998:web:118c30e16a3eb944e237bf"
+    };
 
-//ADD YOUR FIREBASE LINKS HERE
+    if(!firebase.apps.length)
+    {
+        firebase.initializeApp(firebaseConfig);
+    }
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
@@ -8,3 +20,20 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //End code
       });});}
 getData();
+
+
+function logout()
+{
+      window.location = "index.html"
+}
+
+function addRoom()
+{
+      var room_name = document.getElementById("room_name").value;
+
+      localStorage.setItem("room_name", room_name)
+      firebase.database().ref("/").child(room_name).update({
+            what:"talk room"
+      })
+      document.getElementById("room_name").value = "";
+}
